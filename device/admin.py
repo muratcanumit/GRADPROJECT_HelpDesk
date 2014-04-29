e493a1641d5027acecc77340cc8cb2d93df8da53
muratcanumit <muratcanumit@hotmail.com>
2014-04-29 15:01:23 +0300

    admin.py added

--- device/admin.py
+++ device/admin.py
@@ -1,3 +1,31 @@
+from device.models import Device
 from django.contrib import admin
 
-# Register your models here.
+
+class DeviceAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,           {'fields': ['device_type',
+                                     'serial_number',
+                                     'brand_name',
+                                     'model_name',
+                                     'problem',
+                                     'current_status',
+                                     'owner_name',
+                                     'owner_lastname',
+                                     'phone_number',
+                                     'email',
+                                     'khas_id']}),
+        ('Delivery Date', {'fields': ['delivery_date'],
+                           'classes': ['collapse']}),
+    ]
+    list_display = ('survelliance_key', 'device_type',
+                    'brand_name', 'model_name',
+                    'owner_name', 'owner_lastname',
+                    'record_date', 'current_status',
+                    'serial_number')
+    list_filter = ['record_date', 'current_status', 'device_type']
+    search_fields = ['serial_number', 'survelliance_key',
+                     'owner_name', 'owner_lastname', 'brand_name',
+                     'model_name']
+
+admin.site.register(Device, DeviceAdmin)
